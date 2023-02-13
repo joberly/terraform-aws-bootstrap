d70a376ca9ee6fdad121397a3e9cc83fc937b538
John Oberly <joberly@gmail.com>
2023-02-13 01:25:35 +0000

    feat: enable versioning for terraform state bucket

--- main.tf
+++ main.tf
@@ -31,6 +31,13 @@ resource "aws_s3_bucket_acl" "terraform_state" {
   acl    = "private"
 }
 
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "terraform_state" {
   bucket = aws_s3_bucket.terraform_state.id
 
